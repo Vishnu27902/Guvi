66d18f2506ed8e39ae7daeff0009bb7423200955
Vishnu <106140390+Vishnu27902@users.noreply.github.com>
2023-03-18 11:57:34 +0530

    Delete mongo.php

--- mongo.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db="profile.details";
-    $connection=new MongoDB\Driver\Manager("mongodb://localhost:27017");
-    if($connection){
-        $bulk=new MongoDB\Driver\BulkWrite;
-        
-    }
-?>
